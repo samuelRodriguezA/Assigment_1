a8a2bf19bfcd48f0cd462b7b2e087a66d70eab64
samuelRodriguezA <srodriguezarmesto@gmail.com>
2023-10-03 22:53:25 -0400

    Before Making It beautiful

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -9,14 +9,11 @@ res.render('index', { title: 'Home' });
 router.get('/about', function(req, res, next) {
 res.render('about', { title: 'About' });
 });
-router.get('/products', function(req, res, next) {
-res.render('products', { title: 'Products' });
-});
 router.get('/contact', function(req, res, next) {
 res.render('contact', { title: 'Contact' });
 });
-router.get('/services', function(req, res, next) {
-res.render('services', { title: 'Services' });
+router.get('/Projects', function(req, res, next) {
+res.render('Projects', { title: 'Projects' });
 });
 
 module.exports = router;
